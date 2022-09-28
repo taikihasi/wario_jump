280084ab6ed510502a042127996ba452de79c078
fko2247053 <fko2247053@FIC-PCROOM3B-07>
2022-09-28 11:32:57 +0900

    車の表示修正。

--- car.cpp
+++ car.cpp
@@ -2,10 +2,21 @@
 #include "game.h"
 #include "car.h"
 
+namespace
+{
+	// 待ち時間
+	constexpr int kWaitFrameMin = 60;
+	constexpr int kWaitFrameMax = 180;
+	// 車の速度
+	constexpr float kSpeed = -24.0f;
+}
+
 Car::Car()
 {
+
 	m_handle = -1;
 	m_fieldY = 0.0f;
+	m_waitFrame = 0;
 }
 
 void Car::setGraphic(int handle)
@@ -22,14 +33,23 @@ void Car::setup(float fieldY)
 
 	m_vec.x = -16.0f;
 	m_vec.y = 0.0f;
+
+	// 動き始めるまでの時間を設定	1秒から3秒待つ　60フレームから180フレーム
+	m_waitFrame = GetRand(120) + 60;
 }
 
 void Car::update()
 {
+	if (m_waitFrame > 0)
+	{
+		m_waitFrame--;
+		return;
+	}
 	m_pos += m_vec;
 }
 
 void Car::draw()
 {
 	DrawGraphF(m_pos.x, m_pos.y, m_handle, true);
+//	DrawFormatString(0, 0, GetColor(255,255,255), "wait:%d", m_waitFrame);
 }
